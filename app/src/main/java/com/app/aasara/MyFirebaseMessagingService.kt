60b40d9f9aabfe24c7c6b35da69fd0838f7da3b5
Prachip9767 <prachip293@gmail.com>
2021-11-21 09:49:26 +0530

    home and forms

--- app/src/main/java/com/app/aasara/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/app/aasara/MyFirebaseMessagingService.kt
@@ -1,4 +1,148 @@
 package com.app.aasara
 
-class MyFirebaseMessagingService {
+import com.google.firebase.messaging.FirebaseMessagingService
+import android.app.NotificationManager
+
+import android.app.NotificationChannel
+
+import android.os.Build
+
+import android.R
+import android.annotation.SuppressLint
+
+import android.app.PendingIntent
+import android.content.Context
+
+import android.content.Intent
+
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+
+import com.google.firebase.messaging.RemoteMessage
+
+
+
+
+class MyFirebaseMessagingService:FirebaseMessagingService() {
+@SuppressLint("UnspecifiedImmutableFlag")
+  // Override onMessageReceived() method to extract the
+    // title and
+    // body from the message passed in FCM
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // First case when notifications are received via
+        // data event
+        // Here, 'title' and 'message' are the assumed names
+        // of JSON
+        // attributes. Since here we do not have any data
+        // payload, This section is commented out. It is
+        // here only for reference purposes.
+        /*if(remoteMessage.getData().size()>0){
+            showNotification(remoteMessage.getData().get("title"),
+                          remoteMessage.getData().get("message"));
+        }*/
+
+        // Second case when notification payload is
+        // received.
+        if (remoteMessage.notification != null) {
+            // Since the notification is received directly from
+            // FCM, the title and the body can be fetched
+            // directly as below.
+            showNotification(
+                remoteMessage.notification!!.title,
+                remoteMessage.notification!!.body
+            )
+        }
+    }
+
+    // Method to get the custom Design for the display of
+    // notification.
+    private fun getCustomDesign(
+        title: String?,
+        message: String?
+    ): RemoteViews? {
+        val remoteViews = RemoteViews(
+            applicationContext.packageName,
+            R.layout.simple_spinner_item
+        )
+        remoteViews.setTextViewText(R.id.title, title)
+        remoteViews.setTextViewText(R.id.message, message)
+        remoteViews.setImageViewResource(
+            R.id.icon,
+            R.drawable.ic_menu_compass
+        )
+        return remoteViews
+    }
+
+    // Method to display the notifications
+    fun showNotification(
+        title: String?,
+        message: String?
+    ) {
+        // Pass the intent to switch to the MainActivity
+        val intent = Intent(this, AddEventActivity::class.java)
+        // Assign channel ID
+        val channel_id = "notification_channel"
+        // Here FLAG_ACTIVITY_CLEAR_TOP flag is set to clear
+        // the activities present in the activity stack,
+        // on the top of the Activity that is to be launched
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        // Pass the intent to PendingIntent to start the
+        // next Activity
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        // Create a Builder object using NotificationCompat
+        // class. This will allow control over all the flags
+        var builder: NotificationCompat.Builder = NotificationCompat.Builder(
+            applicationContext,
+            channel_id
+        )
+            .setSmallIcon(R.drawable.ic_menu_compass)
+            .setAutoCancel(true)
+            .setVibrate(
+                longArrayOf(
+                    1000, 1000, 1000,
+                    1000, 1000
+                )
+            )
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        // A customized design for the notification can be
+        // set only for Android versions 4.1 and above. Thus
+        // condition for the same is checked here.
+        if (Build.VERSION.SDK_INT
+            >= Build.VERSION_CODES.JELLY_BEAN
+        ) {
+            builder = builder.setContent(
+                getCustomDesign(title, message)
+            )
+        } // If Android Version is lower than Jelly Beans,
+        else {
+            builder = builder.setContentTitle(title)
+                .setContentText(message)
+                .setSmallIcon(R.drawable.ic_menu_compass)
+        }
+        // Create an object of NotificationManager class to
+        // notify the
+        // user of events that happen in the background.
+        val notificationManager = getSystemService(
+            Context.NOTIFICATION_SERVICE
+        ) as NotificationManager
+        // Check if the Android Version is greater than Oreo
+        if (Build.VERSION.SDK_INT
+            >= Build.VERSION_CODES.O
+        ) {
+            val notificationChannel = NotificationChannel(
+                channel_id, "web_app",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(
+                notificationChannel
+            )
+        }
+        notificationManager.notify(0, builder.build())
+    }
 }
