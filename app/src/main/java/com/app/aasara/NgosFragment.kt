e6760a1da1d19e5722809e080c68875757957fb4
Reeta Thakur <thakurreeta@gmail.com>
2021-11-21 14:45:09 +0530

    complete show activity

--- app/src/main/java/com/app/aasara/NgosFragment.kt
+++ app/src/main/java/com/app/aasara/NgosFragment.kt
@@ -1,5 +1,6 @@
 package com.app.aasara
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,15 +10,14 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.aasara.commanInBoth.MainUIModel
-import com.app.aasara.ngoDetail.NgoDetail
-import com.app.aasara.ngoDetail.NgoDetailsAdapter
-import com.app.aasara.ngoDetail.NgoViewModel
+import com.app.aasara.ngoDetail.*
 import kotlinx.android.synthetic.main.activity_ngo_details.*
 
-class NgosFragment : Fragment(R.layout.fragment_ngos) {
+class NgosFragment : Fragment(R.layout.fragment_ngos),FullShow {
     private lateinit var viewModel: NgoViewModel
     private var list= ArrayList<NgoDetail>()
     private lateinit var ngoAdapter : NgoDetailsAdapter
+   // private lateinit var listner:FullShow
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -40,11 +40,16 @@ class NgosFragment : Fragment(R.layout.fragment_ngos) {
     }
 
     private fun setRecyclerView() {
-        ngoAdapter= NgoDetailsAdapter(list)
+        ngoAdapter= NgoDetailsAdapter(list,this@NgosFragment)
         val linearLayoutManager= LinearLayoutManager(context)
         recyclerView.apply {
             adapter=ngoAdapter
             layoutManager=linearLayoutManager
         }
+    }
+
+    override fun showFullDetails(position: Int) {
+        val intent=Intent(context,NgoFullDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
